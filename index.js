ce5953b4f25151b878a260383b6aab5d14345241
Liliya <liliyavoloshine@gmail.com>
2021-07-12 10:32:39 +0400

    refactor: remove test file, delete testing line from class

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const GuessingGame = require('./src/guessing-game.js');
-
-const number = 42
+const GuessingGame = require('./src/guessing-game.js')
 window.game = new GuessingGame()
-game.setRange(0, 100)
-let result = game.guess()
-if (result !== number) {
-  console.log(`No way! ${number} !== ${result}`);
-}
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,28 +1,27 @@
 class GuessingGame {
-    constructor() {
-        this.max = 0
-        this.min = 0
-        this.candidate = 0
-    }
+  constructor() {
+    this.max = 0
+    this.min = 0
+    this.candidate = 0
+  }
 
-    setRange(min, max) {
-        this.max = max
-        this.min = min
-        console.log(this.min, this.max);
-    }
+  setRange(min, max) {
+    this.max = max
+    this.min = min
+  }
 
-    guess() {
-        this.candidate = Math.round((this.min + this.max) / 2)
-        return this.candidate
-    }
+  guess() {
+    this.candidate = Math.round((this.min + this.max) / 2)
+    return this.candidate
+  }
 
-    lower() {
-        this.max = this.candidate
-    }
+  lower() {
+    this.max = this.candidate
+  }
 
-    greater() {
-        this.min = this.candidate
-    }
+  greater() {
+    this.min = this.candidate
+  }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
